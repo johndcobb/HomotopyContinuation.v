c1ccda37a9461d9d8dcf8f1a2bef7019e1770439
John Cobb <jdcobb3@gmail.com>
2025-03-17 12:28:14 -0500

    verilog init

---
+++ Verilog/source/homotopy_Top.v
@@ -0,0 +1,13 @@
+/*
+This is going to be written for systems that have exactly as many functions as there are inputs (nothing underdetermined, overdetermined). default = 2
+It will also need a maximum degree of any polynomial in the system. default = 3
+We also need to maintain how many steps we need in the homotopy process, and a cap on the number of paths to track. 
+All of these are to set a cap on how much data to allocate for the system. 
+*/
+
+module homotopy_Top (
+    input i_Clk,
+
+);
+    
+endmodule
