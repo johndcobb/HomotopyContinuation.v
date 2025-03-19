d08548d925f85f51eaffc11a07d61b8f2d70e57c
John Cobb <jdcobb3@gmail.com>
2025-03-19 12:08:41 -0500

    Add basic example function to demonstrate Homotopy usage with Polynomial structs

--- homotopycontinuationdriver/src/main.rs
+++ homotopycontinuationdriver/src/main.rs
@@ -2,10 +2,23 @@ mod polynomials;
 use polynomials::Polynomial;
 use polynomials::ComplexNumber;
 
-fn main() {
-    let z = ComplexNumber::new(1.0, 2.0);
-    let p = Polynomial::new(vec![1.0,2.0,3.0]);
+mod homotopy;
+use homotopy::Homotopy;
 
-    println!("z = {}", z);
-    println!("p = {}", p);
+fn main() {
+    basic_example();
+}
+
+fn basic_example() {
+    let f = Polynomial::new(vec![1.0, 2.0, 3.0, 4.0]);
+    let g = Polynomial::new(vec![1.0, 0.0, 0.0, -1.0]);
+
+    println!("f(z) = {}", &f);
+    println!("g(z) = {}", &g);
+
+    let h = Homotopy::new(f,g);
+    println!("h(z,0) = {}", &h.time(0.0));
+    println!("h(z,0.5) = {}", &h.time(0.5));
+    println!("h(z,1) = {}", &h.time(1.0));
+    println!("h.random_unit = {}", &h.random_unit);
 }
